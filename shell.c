e8af05284c7690bbe6ede5cb5182a586b3abd8f5
DZEK00 <zakarialyazale7@gmail.com>
2023-05-14 11:31:09 +0100

    update shell.c

--- shell.c
+++ shell.c
@@ -6,37 +6,37 @@
 
 void shell(void)
 {
-    char *line = NULL, *arg[10], *tock;
-    int i = 0, pid;
-    size_t len = 0;
-    ssize_t read;
-    
-    write(1, "($)", 3);
-    read = getline(&line, &len, stdin);
-    while (read != -1)
-    {
-        for (tock = strtok(line, " \t\n\"\'\\#&*`"), i = 0; tock != NULL;
-             tock = strtok(NULL, " \t\n\"\'\\#&*`"), i++)
-        {
-            arg[i] = tock;
-        }
-        arg[i] = NULL;
+	char *line = NULL, *arg[10], *tock;
+	int i = 0, pid;
+	size_t len = 0;
+	ssize_t read;
 
-        pid = fork();
-        if (pid == 0)
-        {
-            if (execve(arg[0], arg, NULL) == -1)
-            {
-                perror("./shell");
-                break;
-            }
-        }
-        else
-        {
-            wait(NULL);
-            write(1, "($)", 3);
-            read = getline(&line, &len, stdin);
-        }
-    }
-    free(line);
+	write(1, "($)", 3);
+	read = getline(&line, &len, stdin);
+	while (read != -1)
+	{
+		for (tock = strtok(line, " \t\n\"\'\\#&*`"), i = 0; tock != NULL;
+				tock = strtok(NULL, " \t\n\"\'\\#&*`"), i++)
+		{
+			arg[i] = tock;
+		}
+		arg[i] = NULL;
+
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(arg[0], arg, NULL) == -1)
+			{
+				perror("./shell");
+				break;
+			}
+		}
+		else
+		{
+			wait(NULL);
+			write(1, "($)", 3);
+			read = getline(&line, &len, stdin);
+		}
+	}
+	free(line);
 }
